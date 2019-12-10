3ac7eb6549934a90e0c0253edff0aab4f19dda5e
J-keven <jhonasnunes425@gmail.com>
2019-12-09 22:08:30 -0300

    add to class bird

---
+++ Bird.py
@@ -0,0 +1,50 @@
+import pygame
+from pygame.locals import *
+import Diretories as D
+
+class Bird(pygame.sprite.Sprite):
+    def __init__(self, SCREENWIDTH, SCREENHEIGTH):
+        pygame.sprite.Sprite.__init__(self)
+        self.__Max_Higth = 0
+        self.__SPEED = 10
+        self.current_imagen = 0
+        self.x = SCREENHEIGTH / 2
+        self.y = SCREENWIDTH / 2
+        self.__Imagens = self.LoadImagens()
+        self.image = pygame.image.load(D.BIRDMID).convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect[0] = self.y
+        self.rect[1] = self.x
+        self.__caindo = True
+
+    def LoadImagens(self):
+        imagens = [
+            pygame.image.load( D.BIRDUP).convert_alpha(),
+            pygame.image.load(D.BIRDMID).convert_alpha(),
+            pygame.image.load( D.BIRDDOWN).convert_alpha()]
+        return imagens
+    
+    def jump(self):
+        self.__Max_Higth = 100
+        self.__caindo = False
+        self.__SPEED = 10
+
+    def update(self):
+        self.current_imagen = (self.current_imagen + 1) % 3 
+        self.image = self.__Imagens[self.current_imagen]
+        if self.rect[1] > -20 and not self.__caindo and self.__Max_Higth > 0:
+            self.__Max_Higth -= self.__SPEED
+            self.rect[1] -= self.__SPEED
+            self.__SPEED -= 1
+
+        else:
+            self.__caindo = True
+            self.rect[1] += 10
+    
+    @property
+    def get_x(self):
+        return self.rect[0]
+
+    @property
+    def get_y(self):
+        return self.rect[1]
